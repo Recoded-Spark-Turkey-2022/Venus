488939adb8cd7506b3cf2d8afd32ae97b541a53a
Dilara Cetinberk <dilaracetinberk@gmail.com>
2022-12-22 13:34:49 +0300

    Style: add
    button text color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,20 +11,20 @@ const Footer = ()=>{
           </div>
           <div className='w-full flex-grow lg:flex lg:items-center lg:w-auto'>
             <ul className="flex justify-center">
-              <li className='mr-6'><Link className='text-[#8b8f9c] hover:text-[#4699c2]' to='/'>Home</Link></li>
-              <li className='mr-6'><Link className='text-[#8b8f9c] hover:text-[#4699c2]' to='/about'>About</Link></li>
-              <li className='mr-6'><Link className='text-[#8b8f9c] hover:text-[#4699c2]' to='/contact'>Contact</Link></li>
-              <li className='mr-6'><Link className='text-[#8b8f9c] hover:text-[#4699c2]' to='/blogs'>Blogs</Link></li>
+              <li className='mr-6'><Link className='text-darkGrey hover:text-darkBlue' to='/'>Home</Link></li>
+              <li className='mr-6'><Link className='text-darkGrey hover:text-darkBlue' to='/about'>About</Link></li>
+              <li className='mr-6'><Link className='text-darkGrey hover:text-darkBlue' to='/contact'>Contact</Link></li>
+              <li className='mr-6'><Link className='text-darkGrey hover:text-darkBlue' to='/blogs'>Blogs</Link></li>
             </ul>
           </div>
           <div className='mx-auto mt-10 sm:mt-1 px-16 sm:px-8 flex lg:justify-center'>
             <div className='flex justify-between flex-row'>
             <div className='flex justify-between items-center mr-3'>
-              <button className='text-white rounded-full bg-mediumBlue px-8 ' type='button' >Sign up</button>
+              <button className='text-[#FFFFFF] rounded-full bg-mediumBlue px-8' type='button' >Sign up</button>
             </div>
             <div className=''>
-              <select className='rounded-full border-[#4699c2] px-8'> 
-                <option className='border-[#4699c2]' value='en'>English</option>
+              <select className='rounded-full border border-solid border-mediumBlue px-8'> 
+                <option className='border-mediumBlue' value='en'>English</option>
                 <option value='ar'>Arabic</option>
               </select>
             </div>
