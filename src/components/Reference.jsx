2271f6d1d1204807d214e5dde436f2bf007a47e4
Dilara Cetinberk <dilaracetinberk@gmail.com>
2022-12-23 16:39:20 +0300

    build: create component

---
+++ src/components/Reference.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import  {Pagination} from 'swiper';
+import 'swiper/swiper-bundle.css';
+import '../index.css'
+import worldMap from '../assets/world_map_ 1.svg'
+
+
+function Reference (){
+
+
+    return(
+        <div className="flex flex-col md:flex-row justify-center items-center py-8 lg:py-20">
+            <div className="pb-8 lg:pb-0 flex justify-center items-center">
+                <img className="w-2/3" src={worldMap} alt="World Map"  />
+            </div>
+            <div className="flex justify-center items-center w-full md:w-1/2 ">
+            <Swiper
+         spaceBetween={30}
+         pagination={{
+           clickable: true,
+         }}
+         modules={[Pagination]}
+         className="mySwiper">
+            <SwiperSlide>
+                <div className="w-1/2 reference-div">
+                    <h3 className="pt-2">Two lines of a short testimonial from someone want to say something, and can
+                     say more to explain.</h3>
+                    <p className="text-darkGrey pt-2">Louis Li</p>
+                    <p className="text-darkGrey text-sm">Trainer at Recoded</p>
+                </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className="w-1/2 reference-div">
+                    <h3 className="pt-2">Two lines of a short testimonial from someone want to say something, and can
+                     say more to explain.</h3>
+                    <p className="text-darkGrey pt-2">Louis Li</p>
+                    <p className="text-darkGrey text-sm">Trainer at Recoded</p>
+                </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className="w-1/2 reference-div">
+                    <h3 className="pt-2">Two lines of a short testimonial from someone want to say something, and can
+                     say more to explain.</h3>
+                    <p className="text-darkGrey pt-2">Louis Li</p>
+                    <p className="text-darkGrey text-sm">Trainer at Recoded</p>
+                </div>
+                </SwiperSlide>
+                </Swiper>
+            </div>
+
+        </div>
+    )
+}
+
+export default Reference;
