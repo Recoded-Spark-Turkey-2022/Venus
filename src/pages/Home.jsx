8df5bfc428798983b187db580250b55a8cd4d066
Dilara Cetinberk <dilaracetinberk@gmail.com>
2022-12-23 16:39:40 +0300

    doc: add component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Container from '../components/UI/Container';
+import Reference from '../components/Reference';
 
 const Home = () => {
   return (
     <Container>
       <h1 className="text-darkOrange bg-lightBlue"> Hello</h1>
+
+      <Reference/>
     </Container>
   );
 };
