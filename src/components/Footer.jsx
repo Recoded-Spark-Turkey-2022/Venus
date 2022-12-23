dc0f205f17233a04eb5ec3d0d892acfe776f4834
Dilara Cetinberk <dilaracetinberk@gmail.com>
2022-12-23 21:24:01 +0300

    style: add button style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,9 @@ const Footer = ()=>{
           <div className='mx-auto mt-10 sm:mt-1 px-16 sm:px-8 flex lg:justify-center'>
             <div className='flex justify-between flex-row'>
             <div className='flex justify-between items-center mr-3'>
-              <button className='text-[#FFFFFF] rounded-full bg-mediumBlue px-8' type='button' >Sign up</button>
+              <button className='text-[#FFFFFF] rounded-full bg-mediumBlue px-8 btn-default transition-colors
+     hover:bg-white ease-in duration-300 border border-mediumBlue
+     hover:border-mediumBlue hover:text-mediumBlue' type='button' >Sign up</button>
             </div>
             <div className=''>
               <select className='rounded-full border border-solid border-mediumBlue px-8'> 
