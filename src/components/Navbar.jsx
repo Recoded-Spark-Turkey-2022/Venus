2f2e3c212c760f0f643bfc472f06537151b1cf1f
tasnimalhallak <tasneemamino133@gmail.com>
2022-12-24 19:19:14 +0300

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Button from './Button';
+import logo from  '../assets/Logo.svg'
 
 const Navbar = () => {
     let Links =[
@@ -15,13 +16,13 @@ const Navbar = () => {
       <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
       text-cyan-600'>
         <span className='text-3xl text-cyan-600 mr-1 pt-2'>
-        <ion-icon name="logo-react"></ion-icon>
+        <img src={logo}></img>
         </span>
-        Refubook
+        
       </div>
       
       <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
-      <ion-icon name={open ? 'close':'menu'}></ion-icon>
+      <img name={open ? 'close':'menu'}></img>
       </div>
 
       <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
