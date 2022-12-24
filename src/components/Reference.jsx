d003ee3eddb843fa08224eaa7a3c930e9af0854d
Dilara Cetinberk <dilaracetinberk@gmail.com>
2022-12-24 14:37:37 +0300

    style: add effect

--- src/components/Reference.jsx
+++ src/components/Reference.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import  {Pagination} from 'swiper';
+import { EffectCreative, Pagination } from "swiper";
 import 'swiper/swiper-bundle.css';
+
 import '../index.css'
 import worldMap from '../assets/world_map_ 1.svg'
 
@@ -10,18 +11,31 @@ function Reference (){
 
 
     return(
-        <div className="flex flex-col md:flex-row justify-evenly items-center py-12 ">
+        <div className="flex flex-col md:flex-row justify-around items-center py-12 ">
             <div className="flex justify-center items-center ">
                 <img className="w-2/3 md:w-full" src={worldMap} alt="World Map"  />
             </div>
             <div className="flex justify-end w-full md:w-1/2 ">
             <Swiper
-         spaceBetween={30}
-         pagination={{
-           clickable: true,
-         }}
-         modules={[Pagination]}
-         className="mySwiper">
+          
+          grabCursor='true'
+        effect="creative"
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        }}
+          loop="true"
+          pagination={{
+            clickable: true,  dynamicBullets: true 
+          }}
+          
+          modules={[EffectCreative,Pagination]}
+          className="mySwiper">
             <SwiperSlide>
                 <div className="w-1/2 reference-div text-center sm:text-left">
                     <h3 className="pt-2">Two lines of a short testimonial from someone want to say something, and can
