79ac9419395d92f13fe3e5266c8d33971458843f
halilibrahimcelik <halil.celik139@gmail.com>
2022-12-25 13:21:35 +0300

    bug:layout styling fix

--- src/components/homePage/HeroSection.jsx
+++ src/components/homePage/HeroSection.jsx
@@ -6,20 +6,20 @@ import Button from '../UI/Button';
 
 const HeroSection = () => {
   return (
-    <section className=" mt-[700px]   min-h-[90vh] relative overflow-hidden md:overflow-visible mb-10">
+    <section className=" mt-[70px]  w-full  min-h-[90vh] relative overflow-hidden md:overflow-visible mb-10">
       <img
         src={circleLogo}
         className="absolute image-hero z-0 left-[-500px] md:left-[-200px] lg:left-[-300px] xl:left-[-400px] 2xl:left-[-500px] top-[0px]  w-[100%] h-[90vh]"
         alt="circle-logo"
       />
-      <div className=" items-center  flex  flex-col-reverse md:flex-row  justify-center md:justify-between lg:justify-between ">
+      <div className=" items-center flex flex-col-reverse md:flex-row   md:w-full justify-between md:justify-between lg:justify-between ">
         <motion.div
           initial={{ x: '-1000px', scale: 0.5 }}
           animate={{ x: '0px', scale: 1 }}
           transition={{ duration: 0.5 }}
           className="text-center md:text-left "
         >
-          <div className="z-10 relative">
+          <div className="z-10 mt-20 md:mt-20  md:mr-10 lg:mr-0">
             <h1 className="text-5xl lg:text-7xl font-bold text-mediumBlue my-5">
               RefuBook
             </h1>
@@ -40,7 +40,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
         >
           <img
-            className="mx-auto  w-full xl:w-[700px] md:mx-0  md:aspect-square"
+            className="mx-auto mt-5 md:mt-0  w-full xl:w-[700px] md:mx-0  md:aspect-square"
             src={logo}
             alt="hero-logo"
           />
