ee73bcf52abf84dd2cb82fe3decddd6c4fc42845
Abdullah-Naifeh <abdna59@gmail.com>
2022-12-27 15:28:17 +0300

    modify text font-size

--- src/components/StoryComponent.jsx
+++ src/components/StoryComponent.jsx
@@ -20,15 +20,15 @@ export default function StoryComponent() {
         <img className='rounded' src={storyDetails.mainImage} alt="" />
       </div>
       <div className='flex-col text-[#ffffff] max-w-[400px] min-h-[140px]'>
-        <button  type="button" className='bg-[#FEDB9B] rounded-[3px] text-[10px] text-[#4699C2] px-[10px] py-[2px] mt-[17px] md:mt-[25px]'>Language</button>
+        <button  type="button" className='bg-[#FEDB9B] rounded-[3px] text-[12px] text-[#4699C2] font-bold leading-4 tracking-[0.3px] px-[10px] py-[2px] mt-[17px] md:mt-[25px]'>Language</button>
         <p className='text-[18px] md:text-4xl font-light mt-[10px] md:mt-[20px] md:drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] max-w-[231px] md:max-w-[400px]'>{storyDetails.description}</p>
         <div className='flex mt-[30px] md:mt-10' width={30}>
           <div className="bg-[#FEDB9B] rounded-full mr-[21px] self-center">
             <img  className=' rounded-full w-[30px] h-[28px] md:w-[35px] md:h-[35px]' src={storyDetails.user.userImage} alt=""/>
           </div>
           <div>
-            <a href="#home" className='mb-[6px] font-bold text-[14px]'>{storyDetails.user.userName}</a>
-            <p className='font-light text-[12px] text-[#E9E9E9]'>Refugee in {storyDetails.user.currentUserCountry}</p>
+            <a href="#home" className='mb-[6px] font-bold text-[16px]'>{storyDetails.user.userName}</a>
+            <p className='font-light text-[14px] text-[#E9E9E9]'>Refugee in {storyDetails.user.currentUserCountry}</p>
           </div>
         </div>
       </div>
