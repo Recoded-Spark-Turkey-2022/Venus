f4ff53fb06c995536268bda14c71f2122c2d4ebf
Abdullah-Naifeh <abdna59@gmail.com>
2022-12-28 16:10:23 +0300

    Modify header and descreption font size on mobile screen

--- src/components/UI/StoriesSection.jsx
+++ src/components/UI/StoriesSection.jsx
@@ -6,8 +6,8 @@ export default function StoriesSection() {
   return (
     <div className='bg-[#4699C2] '>
         <Container>
-        <h1 className='text-[#ffffff] font-normal  mb-[19px] pt-[28px] md:pt-[86px] text-md  md:text-[54px] leading-[19px] md:leading-[63px] text-center md:text-left'>Latest Stories</h1>
-        <p className='text-[#ffffff] text-[14px] md:text-[18px] leading-[30px] max-w-[402px] text-center md:text-left'>Home is behind, the world ahead and there are many paths to tread through shadows to the edge.</p>  
+        <h1 className='text-[#ffffff] font-normal  mb-[19px] pt-[28px] md:pt-[86px] text-[18px]  md:text-[54px] leading-[19px] md:leading-[63px] text-center md:text-left'>Latest Stories</h1>
+        <p className='text-[#ffffff] text-[16px] md:text-[18px] leading-[30px] max-w-[402px] text-center md:text-left'>Home is behind, the world ahead and there are many paths to tread through shadows to the edge.</p>  
         <StoryComponent/>
         <StoryComponent/>
         </Container>
