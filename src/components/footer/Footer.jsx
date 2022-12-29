efe5ee1a01f17c14ecea6b6f48729a926d9dd1f8
Dilara Cetinberk <dilaracetinberk@gmail.com>
2022-12-29 17:03:30 +0300

    build: add test.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 import Logo from '../../assets/Logo.svg';
 
 const Footer = () => {
+
+  function handleChangeLanguage(){
+    
+  }
   return (
     <footer className=" flex   container mx-auto flex-col md:flex-row items-center justify-center bg-white py-10 px-5">
       <div className="flex justify-center items-center  mt-5 mb-5 md:mt-1 md:mb-1 mx-auto flex-shrink-0">
@@ -13,7 +17,7 @@ const Footer = () => {
           <img src={Logo} alt="Refubook-logo" />
         </a>
       </div>
-      <div className="w-full mb-2 md:mb-1 lg:ml-3 flex-grow lg:flex lg:items-center lg:w-auto">
+      <div data-testid='f-links' className="w-full mb-2 md:mb-1 lg:ml-3 flex-grow lg:flex lg:items-center lg:w-auto">
         <ul className="flex gap-8 items-center font-medium justify-center">
           <li className="">
             <Link className="text-darkGrey hover:text-darkBlue" to="/">
@@ -50,7 +54,7 @@ const Footer = () => {
             </button>
           </div>
           <div className="">
-            <select className="rounded-full border border-solid outline-none text-darkGrey font-medium border-mediumBlue px-8">
+            <select onChange={handleChangeLanguage} className="rounded-full border border-solid outline-none text-darkGrey font-medium border-mediumBlue px-8">
               <option className="border-mediumBlue " value="en">
                 English
               </option>
---
+++ src/components/footer/footer.test.jsx
@@ -0,0 +1,39 @@
+import { toBeInTheDocument } from "@testing-library/jest-dom/dist/matchers";
+import { fireEvent, render, screen, within } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import user from '@testing-library/user-event';
+import Footer from "./Footer";
+
+
+test('renders logo correctly', ()=>{
+    render(<Router>
+        <Footer/>
+    </Router>);
+    const websiteLogo = screen.getByRole('img')
+    expect(websiteLogo).toBeInTheDocument()
+})
+
+test('renders footer Links correctly', ()=>{
+    render(<Router>
+        <Footer/>
+    </Router>);
+    const fLinks = screen.getByTestId('f-links')
+    expect(fLinks).toBeInTheDocument()
+})
+
+test('renders sign up button correctly', ()=>{
+    render(<Router>
+        <Footer/>
+    </Router>);
+    const suButton = screen.getByRole('button')
+    expect(suButton).toBeInTheDocument()
+})
+
+test('onChange is called when user change the option', ()=>{
+    render(<Router>
+        <Footer/>
+    </Router>);
+    const selectLang = screen.getByRole('combobox')
+    fireEvent.change(selectLang, {target: {value: 'English'}})
+    expect(selectLang.value).toBe('English')
+})
