f73e4b849777a8f3252acf12900acd0291544fdc
halilibrahimcelik <halil.celik139@gmail.com>
2022-12-29 22:17:01 +0300

    blog-swipper added.

--- src/components/blogs/BlogItem.jsx
+++ src/components/blogs/BlogItem.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
+import './blogs.css';
 
 const BlogItem = (props) => {
-  const { ImageUrl, avatars, text, title, userName } = props;
+  const { ImageUrl, avatars, text, title, userName, fullWidth } = props;
 
   return (
     <div
-      className="rounded 
-    overflow-hidden max-w-[18rem]
+      className={`rounded 
+    overflow-hidden ${fullWidth ? 'w-[60%]' : 'max-w-[18rem]'}
     flex
  
     flex-col
    justify-between
-     shadow-lg"
+     shadow-lg`}
     >
-      <div className="">
+      <div className="grow-0">
         <img
-          className="w-full aspect-square object-cover"
+          className={`w-full ${
+            fullWidth ? 'aspect-[4/1] ' : 'aspect-[2/1.3]'
+          } object-cover`}
           src={ImageUrl}
           alt=""
         />
       </div>
-      <div className="">
+      <div className="grow flex flex-col justify-between">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{title} </div>
-          <p className="text-gray-700 text-base">{text}</p>
+          <p className="text-gray-700 text-base blog-text">{text}</p>
         </div>
-        <div className="px-6 pt-4 pb-2 flex items-center justify-around">
+        <div className="px-6 pt-4 pb-2 flex items-center">
           <img src={avatars[0]} className="rounded-full w-[70px] h-13" alt="" />
           <p className="text-xl ">{userName} </p>
         </div>
--- src/components/blogs/Bloglist.jsx
+++ src/components/blogs/Bloglist.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect } from 'react';
+import { Pagination, Navigation, Autoplay, EffectFade } from 'swiper';
 import { useDispatch, useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/swiper-bundle.css';
+
 import {
   fetchListsing,
   getAllListings,
@@ -14,10 +19,48 @@ const Bloglist = () => {
     dispatch(fetchListsing());
   }, []);
   return (
-    <div className="flex flex-wrap justify-center w-full gap-3">
-      {data?.map((singlePost) => (
-        <BlogItem key={singlePost.userRef} {...singlePost} />
-      ))}
+    <div>
+      <div className="w-full blogs-swipper">
+        <Swiper
+          fadeEffect="fade"
+          modules={[Pagination, Navigation, Autoplay, EffectFade]}
+          className="mySwiper"
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+        >
+          {data?.map((singlePost) => (
+            <SwiperSlide key={singlePost.userRef}>
+              <div className="flex flex-wrap justify-center w-full ">
+                <BlogItem
+                  key={singlePost.userRef}
+                  {...singlePost}
+                  // eslint-disable-next-line react/jsx-boolean-value
+                  fullWidth={true}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className="flex flex-wrap justify-center w-full gap-3">
+        {data?.map((singlePost, index) => {
+          if (index < 4) {
+            return (
+              <BlogItem
+                key={singlePost.userRef}
+                {...singlePost}
+                // eslint-disable-next-line react/jsx-boolean-value
+                fullWidth={false}
+              />
+            );
+            // eslint-disable-next-line no-else-return
+          } else {
+            return null;
+          }
+        })}
+      </div>
     </div>
   );
 };
