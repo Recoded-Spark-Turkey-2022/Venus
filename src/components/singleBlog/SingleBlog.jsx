fb9b508356fccc2013f0f11141420a604034d798
halilibrahimcelik <halil.celik139@gmail.com>
2023-01-04 22:38:47 +0300

    feat:singleBlog page css finish

--- src/components/singleBlog/SingleBlog.jsx
+++ src/components/singleBlog/SingleBlog.jsx
@@ -1,79 +1,76 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { Pagination, Navigation, Autoplay } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  AiOutlineShareAlt,
+  AiFillFacebook,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from 'react-icons/ai';
+import Container from '../UI/Container';
 import { getAllListings } from '../../features/listings/listingSlice';
 import BlogItem from '../blogs/BlogItem';
 import './singleBlog.css';
 
 const SingleBlog = ({ data }) => {
   const dataStore = useSelector(getAllListings);
-  console.log(dataStore);
+  const randomNumber = Math.floor(Math.random() * dataStore.length) - 1;
+  const randomNum1 = randomNumber > 0 ? randomNumber : 0;
+  const randomNum2 = randomNumber > 0 ? randomNumber + 1 : 5;
+  const randomArr = [];
+  randomArr.push(dataStore[randomNum1]);
+  randomArr.push(dataStore[randomNum2]);
+
   const { avatars, title, userName, text, content, ImageUrl } = data[0];
 
-  const navigate = useNavigate();
   return (
-    <div className="flex flex-col-reverse main-container">
-      <article className="flex flex-col ">
-        <h1 className="text-3xl uppercase font-medium text-center">{title} </h1>
-        <img
-          className="aspect-[2/1] object-cover rounded-xl"
-          src={ImageUrl}
-          alt={title}
-        />
-        <p className="flex  align-middle self-center justify-center items-end mb-5">
-          <span>by</span>{' '}
-          <img
-            className="w-[50px] h-[50px] rounded-full"
-            src={avatars[0]}
-            alt="avatar"
-          />{' '}
-          <span> {userName} </span>
-        </p>
-        <div>
-          <h2 className="text-xl font-medium">{text} </h2>
-          <p>{content} </p>
-        </div>
-      </article>
-      <aside className="w-[100%] h-fit ">
-        <Swiper
-          modules={[Pagination, Navigation, Autoplay]}
-          className="mySwiper"
-          // eslint-disable-next-line react/jsx-boolean-value
-          effect="fade"
-          fadeEffect={{ crossFade: true }}
-          // eslint-disable-next-line react/jsx-curly-brace-presence
-          direction="horizontal"
-          pagination={{
-            clickable: true,
-            dynamicBullets: true,
-          }}
-          autoplay={{
-            delay: 5000,
-            reverseDirection: true,
-          }}
-        >
-          {dataStore?.map((singlePost) => (
-            <SwiperSlide
-              key={singlePost.userRef}
-              className="w-full h-full"
-              onClick={() => navigate(`/blogs/${singlePost.userRef}`)}
-            >
-              <div className="flex flex-wrap justify-center w-full blog-wrapper">
-                <BlogItem
-                  key={singlePost.userRef}
-                  {...singlePost}
-                  // eslint-disable-next-line react/jsx-boolean-value
-                  fullWidth={true}
-                />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </aside>
-    </div>
+    <Container>
+      <div className="flex flex-col-reverse gap-10 md:flex-row main-container">
+        <article className="flex flex-col w-full md:w-[60%]">
+          <h1 className="text-3xl uppercase font-medium text-center mb-4">
+            {title}{' '}
+          </h1>
+          <div className="relative">
+            <img
+              className="aspect-[2/1] object-cover rounded-xl"
+              src={ImageUrl}
+              alt={title}
+            />
+            <div className=" flex flex-col gap-5 absolute top-3 right-5">
+              <AiOutlineShareAlt className="text-white text-3xl font-bold duration-300 brightness-110 hover:brightness-50 cursor-pointer" />{' '}
+              <AiFillFacebook className="text-white text-3xl font-bold duration-300 brightness-110 hover:brightness-50 cursor-pointer" />{' '}
+              <AiFillInstagram className="text-white text-3xl font-bold duration-300 brightness-110 hover:brightness-50 cursor-pointer" />{' '}
+              <AiOutlineTwitter className="text-white text-3xl font-bold duration-300 brightness-110 hover:brightness-50 cursor-pointer" />
+            </div>
+          </div>
+          <p className="flex  align-middle self-center justify-center items-end mb-5">
+            <span>by</span>{' '}
+            <img
+              className="w-[50px] h-[50px] rounded-full"
+              src={avatars[0]}
+              alt="avatar"
+            />{' '}
+            <span> {userName} </span>
+          </p>
+          <div>
+            <h2 className="text-xl font-medium my-3">{text} </h2>
+            <p>{content} </p>
+          </div>
+        </article>
+        <aside className="w-full md:w-[40%] h-fit ">
+          <h2 className="text-2xl font-medium text-center mb-4">Read Also</h2>
+          <div className="flex blog-aside flex-wrap justify-center gap-10 w-full blog-wrapper">
+            {randomArr?.map((singlePost) => (
+              <BlogItem
+                key={singlePost.userRef}
+                {...singlePost}
+                // eslint-disable-next-line react/jsx-boolean-value
+                fullWidth={true}
+              />
+            ))}
+          </div>
+        </aside>
+      </div>
+    </Container>
   );
 };
 
