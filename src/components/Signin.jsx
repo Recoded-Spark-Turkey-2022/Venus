9c8b86da6d7cfd4f97c324f31a147351b716119d
tasnimalhallak <tasneemamino133@gmail.com>
2023-01-10 18:50:17 +0300

    linkPages

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img from '../assets/signup-vector.svg'
 import './Signup-in.css'
+
 
 
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,6 +7,7 @@ import './Signup-in.css'
 
 
 
+
 const Signup = () => {
     return (
         <section className=' w-screen h-screen relative '>
