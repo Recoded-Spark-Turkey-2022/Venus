cd5e3100bb2ad1f7699d6257a815ecadb16e5020
Saeed-Alsaiyed <msalsaiyed0@gmail.com>
2023-01-12 09:39:13 +0300

    style ourstory section

--- src/components/aboutPage/OurStory.jsx
+++ src/components/aboutPage/OurStory.jsx
@@ -7,7 +7,7 @@ export default function OurStory() {
         <h1 className='mt-[37px] md:mt-[126px] mb-[14px] md:mb-[43px] text-[32px] md:text-[64px] text-[#025594] font-bold leading-[38px] md:leading-[75px]'>Our Story</h1>
         </div>
         <div className='mb-[50px] md:mb-[129px]'>
-         <p className='max-w-[312px] md:max-w-[817px] text-center text-[16px] md:text-[24px] font-normal leading-4 md:leading-[36px]'>
+         <p className='max-w-[312px] md:max-w-[817px] text-center text-[16px] md:text-[24px] font-normal leading-loose md:leading-[36px]'>
          At RefuBook, we understand the challenges that come with being a refugee and that is why we are dedicated to providing comprehensive support and resources to individuals who have been forced to flee their homes due to conflict, persecution, or other life-threatening circumstances. We believe that every refugee deserves the opportunity to thrive in their new surroundings. 
          <br /> 
          <br />
