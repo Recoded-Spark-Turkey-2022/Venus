5d353a428bbcd2f065017fda98836c43d0584328
halilibrahimcelik <halil.celik139@gmail.com>
2023-01-24 00:45:55 +0300

    fix:bug

--- src/components/UI/StoriesSection.jsx
+++ src/components/UI/StoriesSection.jsx
@@ -12,7 +12,7 @@ import Container from './Container';
 export default function StoriesSection() {
   const dispatch = useDispatch();
   const data = useSelector(getAllListings);
-  console.log(data);
+
   const selectedBlog1 = data?.filter(
     (singleBlog) => singleBlog.userRef === 'tgK9dBd3xW0F0uUqVxRO'
   );
@@ -31,10 +31,10 @@ export default function StoriesSection() {
     <div className="bg-[#4699C2] ">
       <Container>
         <h1 className="text-[#ffffff] font-normal  mb-[19px] pt-[28px] md:pt-[86px] text-[18px]  md:text-[54px] leading-[19px] md:leading-[63px] text-center md:text-left">
-          {t('Home.last-stories')}
+          {/* {t('Home.last-stories')} */}
         </h1>
         <p className="text-[#ffffff] text-[16px] md:text-[18px] leading-[30px] max-w-[402px] text-center md:text-left">
-          {t('Home.last-stories-p')}
+          {/* {t('Home.last-stories-p')} */}
         </p>
         {selectedBlogs?.map((blog) => {
           return <StoryComponent key={blog.userRef} data={blog} />;
