3b286bd3bbe42015e1ce5abcb19149dcb66bd681
halilibrahimcelik <halil.celik139@gmail.com>
2023-01-24 00:43:07 +0300

    bug:test validation fix

--- src/components/story/StoryComponent.jsx
+++ src/components/story/StoryComponent.jsx
@@ -15,7 +15,6 @@ import userIcon from '../../assets/man.png';
 // };
 
 export default function StoryComponent({ data }) {
-  const { text, avatars, userName, userRef } = data;
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 md:gap-10 mt-[50px] md:mt-[78px] pb-[24px] mr-[80px] ml-[89px] md:mx-[0px]">
       <div>
@@ -26,25 +25,25 @@ export default function StoryComponent({ data }) {
         />
       </div>
       <div className="flex-col text-[#ffffff]">
-        <Link to={`/blogs/${userRef}`}>
+        <Link to={`/blogs/${data?.userRef}`}>
           <div className="bg-[#FEDB9B] text-left rounded-[3px] text-[14px] relative duration-500 ease-in after:content-['&#x2192;'] after:absolute after:opacity-0 hover:after:opacity-100 after:duration-300 after:ease-in  hover:after:right-1 after:text-[20px] after:font-extrabold after:right-[0px] z-10 after:bottom-[6px]  hover:w-[8rem]  w-[6rem] text-[#4699C2] font-bold leading-4 tracking-[0.3px] px-[10px] py-[5px] mt-[17px] md:mt-[25px]">
             Read More
           </div>
         </Link>
         <p className="text-[18px]  first-letter:uppercase  md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-light mt-[10px] md:mt-[20px] md:drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
-          {text}
+          {data?.text}
         </p>
         <div className="flex mt-[30px] md:mt-10" width={30}>
           <div className="bg-[#FEDB9B] rounded-full mr-[21px] self-center">
             <img
               className=" rounded-full w-[30px] h-[28px] md:w-[35px] md:h-[35px]"
-              src={avatars[0] || userIcon}
+              src={data?.avatars[0] || userIcon}
               alt=""
             />
           </div>
           <div>
             <a href="#home" className="mb-[6px] font-bold text-[16px]">
-              {userName}
+              {data?.userName}
             </a>
             <p className="font-light text-[14px] text-[#E9E9E9]">
               Refugee in Turkey
