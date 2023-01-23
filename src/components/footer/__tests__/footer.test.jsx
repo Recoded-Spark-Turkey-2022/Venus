ff44ef221fb11e28146ce9cad8ab5463e3cfdefb
Dilara Cetinberk <dilaracetinberk@gmail.com>
2023-01-23 13:03:23 +0300

    delete comment

--- src/components/footer/__tests__/footer.test.jsx
+++ src/components/footer/__tests__/footer.test.jsx
@@ -15,35 +15,3 @@ it(`renders correctly` , () => {
     expect(tree).toMatchSnapshot();
 });
 
-/* test('renders logo correctly', ()=>{
-    render(<Router>
-        <Footer/>
-    </Router>);
-    const websiteLogo = screen.getByRole('img')
-    expect(websiteLogo).toBeInTheDocument()
-})
-
-test('renders footer Links correctly', ()=>{
-    render(<Router>
-        <Footer/>
-    </Router>);
-    const fLinks = screen.getByTestId('f-links')
-    expect(fLinks).toBeInTheDocument()
-})
-
-test('renders sign up button correctly', ()=>{
-    render(<Router>
-        <Footer/>
-    </Router>);
-    const suButton = screen.getByRole('button')
-    expect(suButton).toBeInTheDocument()
-})
-
-test('onChange is called when user change the option', ()=>{
-    render(<Router>
-             <Footer/>
-           </Router>);
-    const selectLang = screen.getByRole('combobox')
-    fireEvent.change(selectLang, {target: {value: 'English'}})
-    expect(selectLang.value).toBe('English')
-}) */
