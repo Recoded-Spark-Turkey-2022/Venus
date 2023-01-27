6313c4916dbad2006e1c02b796d6a1be0a44111c
Dilara Cetinberk <dilaracetinberk@gmail.com>
2023-01-27 20:17:39 +0300

    update navbar test

--- src/components/navbar/Navbar.test.jsx
+++ src/components/navbar/Navbar.test.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Provider } from 'react-redux';
+import { store } from '../../app/store';
 import renderer from "react-test-renderer";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Navbar";
@@ -11,21 +13,18 @@ jest.mock('react-i18next', () => ({
   },
 }));
 
-  jest.mock('react-redux', () => ({
-    useSelector: () => ({
-      user: {
-        isLoggedIn: 'user',
-      },
-    }),
-    useDispatch: () => jest.fn(),
-  }));
   
   describe('Navbar', () => {
     it('renders correctly', () => {
-      const tree = renderer.create(
-      <Router>
-        <Navbar />
-        </Router>).toJSON();
+      const tree = renderer
+        .create(
+          <Router>
+            <Provider store={store}>
+              <Navbar />
+            </Provider>
+          </Router>
+        )
+        .toJSON();
       expect(tree).toMatchSnapshot();
     });
   });
