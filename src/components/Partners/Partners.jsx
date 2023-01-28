7defbbcbc3bbd39162e3c4417e2393a21a0b84c7
halilibrahimcelik <halil.celik139@gmail.com>
2023-01-28 13:47:46 +0300

    feat:animation added to homepage

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,103 +1,102 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {Pagination,  Autoplay} from "swiper";
+import { Pagination, Autoplay } from 'swiper';
 import { useTranslation } from 'react-i18next';
-import p1 from '../../assets/1.png'
-import p2 from '../../assets/2.png'
-import p3 from '../../assets/3.png'
-import p4 from '../../assets/4.png'
-import p5 from '../../assets/5.png'
-import p6 from '../../assets/6.png'
-import p7 from '../../assets/7.png'
-import p8 from '../../assets/8.png'
+import { motion } from 'framer-motion';
+import p1 from '../../assets/1.png';
+import p2 from '../../assets/2.png';
+import p3 from '../../assets/3.png';
+import p4 from '../../assets/4.png';
+import p5 from '../../assets/5.png';
+import p6 from '../../assets/6.png';
+import p7 from '../../assets/7.png';
+import p8 from '../../assets/8.png';
 import 'swiper/swiper-bundle.css';
-import './Partners.css'
+import './Partners.css';
 
-
-function Partners(){
+function Partners() {
   const { t } = useTranslation();
-    return(
-      <div>
-        
-        <div className='mt-4'>
-            <div className='text-partners flex items-center justify-center text-xl font-bold'><h4>{t("Home.partners")}</h4></div>
-            <div className='swiper-div flex justify-center  items-center w-full  px-6 mt-0 md:mt-5 pb-2'>
-            <Swiper
-    autoplay={{
-      delay: 3000,
-      disableOnInteraction: false,
-    }}
-    breakpoints={{
-      320: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
-      1024: {
-        slidesPerView: 5,
-        spaceBetween: 30,
-      }
-      
-    }}
-
-      pagination={{
-        clickable: true,  dynamicBullets: true 
-      }}
-      
-      modules={[Pagination, Autoplay]}
-      className="mySwiper w-full"
-        
-      >
-         <SwiperSlide>
-          <div className=''>
-            <img   className=" " src={p1} alt=''/>
-          </div>
-         </SwiperSlide>
-         <SwiperSlide>
-          <div className=''>
-            <img  className=" " src={p2} alt=''/>
-          </div>
-         </SwiperSlide>
-         <SwiperSlide>
-          <div className=''>
-            <img  className=" " src={p3} alt=''/>
-          </div>
-         </SwiperSlide>
-         <SwiperSlide>
-          <div className=''>
-            <img  className=" " src={p4} alt=''/>
-          </div>
-         </SwiperSlide>
-         <SwiperSlide>
-          <div className=''>
-            <img  className=" " src={p5} alt=''/>
-          </div>
-         </SwiperSlide>
-         <SwiperSlide>
-          <div className=''>
-            <img  className=" " src={p6} alt=''/>
-          </div>
-         </SwiperSlide>
-         <SwiperSlide>
-          <div className=''>
-            <img  className=" " src={p7} alt=''/>
-          </div>
-         </SwiperSlide>
-         <SwiperSlide>
-          <div className=''>
-            <img  className=" " src={p8} alt=''/>
-          </div>
-         </SwiperSlide>
-        
-      </Swiper>
-      </div>
-      </div>
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      transition={{ duration: 0.7, staggerChildren: 0.5 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      viewport={{ once: false, amount: 0.5 }}
+    >
+      <div className="mt-4">
+        <div className="text-partners flex items-center justify-center text-xl font-bold">
+          <h4>{t('Home.partners')}</h4>
         </div>
-        
-
-    )
-
+        <div className="swiper-div flex justify-center  items-center w-full  px-6 mt-0 md:mt-5 pb-2">
+          <Swiper
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            breakpoints={{
+              320: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 4,
+                spaceBetween: 30,
+              },
+              1024: {
+                slidesPerView: 5,
+                spaceBetween: 30,
+              },
+            }}
+            pagination={{
+              clickable: true,
+              dynamicBullets: true,
+            }}
+            modules={[Pagination, Autoplay]}
+            className="mySwiper w-full"
+          >
+            <SwiperSlide>
+              <div className="">
+                <img className=" " src={p1} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="">
+                <img className=" " src={p2} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="">
+                <img className=" " src={p3} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="">
+                <img className=" " src={p4} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="">
+                <img className=" " src={p5} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="">
+                <img className=" " src={p6} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="">
+                <img className=" " src={p7} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="">
+                <img className=" " src={p8} alt="" />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
 export default Partners;
--- src/components/UI/StoriesSection.jsx
+++ src/components/UI/StoriesSection.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
 
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -12,7 +14,7 @@ import Container from './Container';
 export default function StoriesSection() {
   const dispatch = useDispatch();
   const data = useSelector(getAllListings);
-
+  const { t } = useTranslation();
   const selectedBlog1 = data?.filter(
     (singleBlog) => singleBlog.userRef === 'tgK9dBd3xW0F0uUqVxRO'
   );
@@ -28,18 +30,28 @@ export default function StoriesSection() {
   }, []);
 
   return (
-    <div className="bg-[#4699C2] ">
-      <Container>
-        <h1 className="text-[#ffffff] font-normal  mb-[19px] pt-[28px] md:pt-[86px] text-[18px]  md:text-[54px] leading-[19px] md:leading-[63px] text-center md:text-left">
-          {/* {t('Home.last-stories')} */}
-        </h1>
-        <p className="text-[#ffffff] text-[16px] md:text-[18px] leading-[30px] max-w-[402px] text-center md:text-left">
-          {/* {t('Home.last-stories-p')} */}
-        </p>
-        {selectedBlogs?.map((blog) => {
-          return <StoryComponent key={blog.userRef} data={blog} />;
-        })}
-      </Container>
-    </div>
+    <AnimatePresence>
+      <motion.div className="bg-[#4699C2] ">
+        <Container>
+          <h1 className="text-[#ffffff] font-normal  mb-[19px] pt-[28px] md:pt-[86px] text-[18px]  md:text-[54px] leading-[19px] md:leading-[63px] text-center md:text-left">
+            {t('Home.last-stories')}
+          </h1>
+          <p className="text-[#ffffff] text-[16px] md:text-[18px] leading-[30px] max-w-[402px] text-center md:text-left">
+            {t('Home.last-stories-p')}
+          </p>
+          <motion.div
+            initial={{ opacity: 0 }}
+            transition={{ delayChildren: 10, staggerChildren: 0.7 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: false, amount: 0.1 }}
+            className="bg-[#4699C2] "
+          >
+            {selectedBlogs?.map((blog) => {
+              return <StoryComponent key={blog.userRef} data={blog} />;
+            })}
+          </motion.div>
+        </Container>
+      </motion.div>
+    </AnimatePresence>
   );
 }
