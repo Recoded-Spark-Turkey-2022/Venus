5585d73d33a2a62b8d6603970b26f303e106026d
halilibrahimcelik <halil.celik139@gmail.com>
2023-01-29 23:16:10 +0300

    feat:bug animation fix

--- src/components/aboutPage/Cover.jsx
+++ src/components/aboutPage/Cover.jsx
@@ -1,17 +1,19 @@
-import React from "react";
-import AboutUs from '../../assets/aboutUs.png'
+import React from 'react';
+import { motion } from 'framer-motion';
+import AboutUs from '../../assets/aboutUs.png';
 
+function Cover() {
+  return (
+    <motion.div
+      initial={{ scale: 0.5 }}
+      whileInView={{ scale: 1 }}
+      transition={{ duration: 0.6, ease: 'linear' }}
+      viewport={{ once: false, amount: 0.1 }}
+      className="flex justify-center"
+    >
+      <img src={AboutUs} alt="/" className="w-full h-1/2" />
+    </motion.div>
+  );
+}
 
-
-function Cover(){
-    return (
-
-        <div className='flex justify-center'>
-
-            <img src={AboutUs} alt="/" className='w-full h-1/2'/>
-
-        </div>
-    );
-};
-
-export default Cover
+export default Cover;
