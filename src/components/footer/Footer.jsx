9f791dfa3ed6a2a0599357dbb1eb559c3095a228
halilibrahimcelik <halil.celik139@gmail.com>
2023-01-30 21:35:08 +0300

    feat:favicon add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
-import { Flip, ToastContainer} from 'react-toastify';
+import { Flip, ToastContainer } from 'react-toastify';
 import { selectUserLoggedIn } from '../../features/userSlice/userSlice';
 import Logo from '../../assets/Logo.svg';
 
-const Footer = ({message}) => {
-
+const Footer = ({ message }) => {
   const { t } = useTranslation();
   const [lang, setLang] = useState({ lang: 'en', dir: 'ltr' });
- 
+
   const handleLanguageChange = (e) => {
-	
     const selected = e.target.value;
     if (selected === 'ar') {
       setLang({ lang: 'ar', dir: 'rtl' });
@@ -23,10 +21,9 @@ const Footer = ({message}) => {
     }
 
     i18next.changeLanguage(selected);
-  
-	};
+  };
   const isUserLoggedIn = useSelector(selectUserLoggedIn);
-  
+
   return (
     <footer className=" relative z-10 flex   container mx-auto flex-col md:flex-row items-center justify-center bg-white py-10 px-5">
       <div className="flex justify-center items-center  mt-5 mb-5 md:mt-1 md:mb-1 mx-auto flex-shrink-0">
@@ -37,26 +34,29 @@ const Footer = ({message}) => {
           <img src={Logo} alt="Refubook-logo" />
         </a>
       </div>
-      <div data-testid='f-links' className="w-full mb-2 md:mb-1 lg:ml-3 flex-grow lg:flex lg:items-center lg:w-auto">
+      <div
+        data-testid="f-links"
+        className="w-full mb-2 md:mb-1 lg:ml-3 flex-grow lg:flex lg:items-center lg:w-auto"
+      >
         <ul className="flex gap-8 items-center font-medium justify-center">
           <li className="">
             <Link className="text-darkGrey hover:text-darkBlue" to="/">
-             {t("Footer.home")} 
+              {t('Footer.home')}
             </Link>
           </li>
           <li className="">
             <Link className="text-darkGrey hover:text-darkBlue" to="/about">
-            {t("Footer.about")}
+              {t('Footer.about')}
             </Link>
           </li>
           <li className="">
             <Link className="text-darkGrey hover:text-darkBlue" to="/contact">
-            {t("Footer.contact")}
+              {t('Footer.contact')}
             </Link>
           </li>
           <li className="">
             <Link className="text-darkGrey hover:text-darkBlue" to="/blogs">
-            {t("Footer.blogs")}
+              {t('Footer.blogs')}
             </Link>
           </li>
         </ul>
@@ -64,9 +64,9 @@ const Footer = ({message}) => {
       <div className="mx-auto mt-5 sm:mt-1 flex lg:justify-center">
         <div className="flex justify-between flex-row">
           <div className="flex justify-between items-center mr-3">
-          {isUserLoggedIn && (
+            {isUserLoggedIn && (
               <button
-              onClick={message}
+                onClick={message}
                 id="mediumBlue-button"
                 className="px-8 whitespace-nowrap rounded-full"
                 type="button"
@@ -75,20 +75,22 @@ const Footer = ({message}) => {
               </button>
             )}
             {!isUserLoggedIn && (
-            <Link to="/signup">
-              <button
-                id="mediumBlue-button"
-                className="px-8 whitespace-nowrap rounded-full"
-                type="button"
-              >
-                {t("Button.su")}
-              </button>
-            </Link>
+              <Link to="/signup">
+                <button
+                  id="mediumBlue-button"
+                  className="px-8 whitespace-nowrap rounded-full"
+                  type="button"
+                >
+                  {t('Button.su')}
+                </button>
+              </Link>
             )}
-
           </div>
           <div className="">
-            <select onChange={handleLanguageChange} className="rounded-full border border-solid outline-none text-darkGrey font-medium border-mediumBlue px-8" >
+            <select
+              onChange={handleLanguageChange}
+              className="rounded-full border border-solid outline-none text-darkGrey font-medium border-mediumBlue px-8"
+            >
               <option className="border-mediumBlue " value="en">
                 English
               </option>
@@ -102,7 +104,6 @@ const Footer = ({message}) => {
       </Helmet>
       <ToastContainer transition={Flip} limit={3} />
     </footer>
-    
   );
 };
 export default Footer;
